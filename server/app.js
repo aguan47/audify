082de31bafdc98f572a4d648322c1f6b095346ec
aguan47 <aguan47@gmail.com>
2022-06-25 01:01:31 +0800

    Creates audio folder automatically if it doesn't exists

--- server/app.js
+++ server/app.js
@@ -2,10 +2,14 @@ require('dotenv').config({path: `./.env.${process.env.NODE_ENV}`});
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
-const path = require('path');
+const fs = require('fs');
 const app = express();
 const { errorHandler } = require('./middleware/ErrorHandling.js');
 
+// Create 'audio' folder if it doesn't exists
+if (!fs.existsSync('./audio')) {
+  fs.mkdirSync('./audio');
+}
 
 // Import all of the routes specified in the routes folder
 const userRoutes = require('./api/routes/Users.js');
@@ -17,29 +21,26 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 
+
+// Use all of the routes specified
+app.use('/users', userRoutes);
+app.use('/journals', journalRoutes);
+
+// Error Handling goes here.
+app.use(errorHandler);
+
+// Serve static images and audios
+app.use('/images', express.static(__dirname + '/images'))
+app.use('/audio', express.static(__dirname + '/audio'));
+    
 // Start listening
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "build")));
-    app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "build", "index.html"));
-    });
     app.listen(process.env.PORT, () =>
       console.log(
         `Listening on port ${process.env.PORT}, ${process.env.NODE_ENV} build`
       )
     );
   } else {
-    // Use all of the routes specified
-    app.use('/users', userRoutes);
-    app.use('/journals', journalRoutes);
-
-    // Error Handling goes here.
-    app.use(errorHandler);
-
-    // Serve static images and audios
-    app.use('/images', express.static(__dirname + '/images'))
-    app.use('/audio', express.static(__dirname + '/audio'));
-    
     app.listen(process.env.PORT, () =>
       console.log(
         `Listening on port ${process.env.PORT}, ${process.env.NODE_ENV} build`
